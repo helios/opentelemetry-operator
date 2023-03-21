eda273c9cd32a3dda240c652578e27424a232e9a
LironKS <liron@gethelios.dev>
2023-03-21 14:32:32 +0200

    linting

--- autoinstrumentation/nodejs/src/autoinstrumentation.ts
+++ autoinstrumentation/nodejs/src/autoinstrumentation.ts
@@ -1,13 +1,12 @@
-import { initialize } from 'helios-opentelemetry-sdk';
+import { initialize } from "helios-opentelemetry-sdk";
 
-const apiToken = process.env.HS_TOKEN || ''
-const serviceName = process.env.HS_SERVICE || ''
-const environment = process.env.HS_ENVIRONMENT || ''
+const apiToken = process.env.HS_TOKEN || "";
+const serviceName = process.env.HS_SERVICE || "";
+const environment = process.env.HS_ENVIRONMENT || "";
 
 initialize({
-    apiToken,       
-    serviceName, 
-    environment,
-    enable: true
- });
- 
+  apiToken,
+  serviceName,
+  environment,
+  enable: true,
+});
