5b9661b8cb6abd9b9f0cd2dbe51ce6ca6b46fda2
LironKS <liron@gethelios.dev>
2023-08-06 14:48:17 +0300

    set static commit

--- .ci/create-release-github.sh
+++ .ci/create-release-github.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-OPERATOR_VERSION=$(git describe --tags)
+# OPERATOR_VERSION=$(git describe --tags)
 
 gh config set prompt disabled
 gh release create \
-    -t "Release ${OPERATOR_VERSION}" \
-    "${OPERATOR_VERSION}" \
+    -t "Release 0.0.1" \
+    "0.0.1" \
     'dist/opentelemetry-operator.yaml#Installation manifest for Kubernetes'
